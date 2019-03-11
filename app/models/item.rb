27c776582636caac8ed09159937229890ec44b78
jckylalaina <jlrandrianarivony@gmail.com>
2019-03-11 11:06:37 +0300

    modif model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+	validates :title , presence:true, :length => {:in => 6..35}
+	validates :description ,presence:true, :length => {:in => 20..250}
+    validates :price,presence:true
 end
