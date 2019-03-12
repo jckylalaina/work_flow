46c714bd454019d9db63eb17e73d9d4f0c4b89cb
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-03-12 10:38:05 +0300

    the migrate of the order table

--- db/migrate/20190311194728_create_orders.rb
+++ db/migrate/20190311194728_create_orders.rb
@@ -1,8 +1,9 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.integer :total_price
+      t.decimal :fee
       t.belongs_to :cart, index: true
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
