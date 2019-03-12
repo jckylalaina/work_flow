0794a6bf70a4764c2090ab90e9d59547c158c79d
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-03-12 09:06:56 +0300

    creation of the model of the order

--- db/migrate/20190311194728_create_orders.rb
+++ db/migrate/20190311194728_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.string :total_price
+      t.integer :total_price
+      t.belongs_to :cart, index: true
       t.timestamps
     end
   end
